ee235d71707a4d841e0e3382ff35d013bc7bd0b5
Peter and Kyle <student@appacademy.io>
2013-11-04 17:58:34 -0800

    name changes, user list

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -11,17 +11,33 @@
 	}
 
 	$(document).ready(function () {
-	  var socket = io.connect('http://localhost');
-	   socket.on('message', function (sOb) {
-			 var text = sOb["text"];
-	     console.log(text);
-			 updateMsgs(text);
-	   });
+		var socket = io.connect('http://localhost');
+		socket.on('message', function (sOb) {
+			var text = sOb["text"];
+			console.log(text);
+			updateMsgs(text);
+		});
+		socket.on('nicknameChangeResult', function (result) {
+			var text = result["message"];
+			if (result["success"]) {
+				updateMsgs(text);
+			} else {
+				alert(text);
+			}
+		});
 		chat = ChatApp.chat = ChatApp.chat || new ChatApp.Chat(socket);
 		$("form.input-form").on("submit", function(event) {
 			event.preventDefault();
 			var newMsg = getMsg(event.target[0].value);
 			chat.sendMessage(newMsg);
+		});
+
+		socket.on('userList', function (userArray) {
+			console.log(userArray);
+			users = userArray.join(", ");
+			var usersEl = $("<h4>Users: </h4>");
+			usersEl.append(users);
+			$("div.user-list").html(usersEl);
 		});
 	});
 })(this);
